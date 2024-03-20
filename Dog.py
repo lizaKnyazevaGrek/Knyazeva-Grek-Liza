f5162ccfa4c448f58f4d8bd93303cc504897165a
lizaKnyazevaGrek <158200293+lizaKnyazevaGrek@users.noreply.github.com>
2024-03-20 10:02:36 +0000

    add to main.py

---
+++ Dog.py
@@ -0,0 +1,15 @@
+class DogSpike:
+   name = 'Spike'
+   
+   def __init__(self):
+      self.is_walked = False 
+      
+   def walked(self):
+      print('Dog walked')
+      self.is_walked = True 
+    
+my_dog = DogSpike
+
+print(my_dog.name)
+print(my_dog.is_walked)
+# my_dog.walk()
