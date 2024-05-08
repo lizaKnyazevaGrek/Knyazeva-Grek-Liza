06191386ad600685d5b30abea790bfdb8f91ca7a
lizaKnyazevaGrek <158200293+lizaKnyazevaGrek@users.noreply.github.com>
2024-05-08 08:36:45 +0000

    add hw10.0805

--- HW10.py
+++ HW10.py
@@ -1,21 +1,21 @@
 class DogRex:
+    name = 'Rex'
     def __init__(self):
-        self.name = 'DogRex'
         self.voice = 'woof-woof'
-        self.hungry = False
+        self.hungry = True
 
     def feed(self):
-        self.hugry= True
+        self.hungry= False
     def walk(self):
-        self.hungry = False 
+        self.hungry = True
 
 
 my_dog = DogRex()
 
 print('my dog name:', my_dog.name)
 print('my dog voice:', my_dog.voice)
-print('my dog is not hungry:', my_dog.hungry)
+print('my dog is hungry:', my_dog.hungry)
 my_dog.feed()
-print('my dog is not hungry:', my_dog.hungry)
+print('my dog is hungry:', my_dog.hungry)
 my_dog.walk()
-print('my dog is not hngry:', my_dog.hungry)
+print('my dog is hugry:', my_dog.hungry)
---
+++ hw9.py
@@ -0,0 +1,5 @@
+def coincides(word, symbol):
+    count = 0
+    for x in word:
+        if x == symbol:
+            count 
